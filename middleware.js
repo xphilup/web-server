c5ca3f9dde515f64c9a56174583ece6f2652212e
Phillip Hamilton <xphilup@hotmail.com>
2017-01-19 08:08:42 -0800

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,17 @@
+
+
+var middleware = {
+	requiredAuthentication: function(req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next) {
+		// var cdate = new Date().toString();
+		console.log('Request: ' + new Date().toString() + ' ' +req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+
+
+module.exports = middleware
--- server.js
+++ server.js
@@ -3,20 +3,9 @@ var express = require('express');
 var app = express();
 var PORT = 3000
 
-var middleware = {
-	requiredAuthentication: function(req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req, res, next) {
-		// var cdate = new Date().toString();
-		console.log('Request: ' + new Date().toString() + ' ' +req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
-
 
 app.get('/about', middleware.requiredAuthentication, function(req, res) {
 	res.send('About us!');
